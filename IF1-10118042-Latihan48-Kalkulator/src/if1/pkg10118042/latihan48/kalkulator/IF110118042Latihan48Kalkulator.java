e1274005ff8b27363541420a1ddbb37ca3d6cf8e
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-11-07 19:30:32 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan48-Kalkulator/src/if1/pkg10118042/latihan48/kalkulator/IF110118042Latihan48Kalkulator.java
@@ -0,0 +1,28 @@
+
+package if1.pkg10118042.latihan48.kalkulator;
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi sebagai Kalkulator
+ */
+public class IF110118042Latihan48Kalkulator {
+
+    
+    public static void main(String[] args) {
+     
+        Kalkulator objKal = new Kalkulator();
+        objKal.setValue1(7.0);
+        objKal.setValue2(5.0);
+        System.out.println("VALUE 1 = "+objKal.getValue1());
+        System.out.println("VALUE 2 = "+objKal.getValue2());
+        objKal.setNameProject();
+        objKal.setNoteProject("");
+        System.out.println("Result Add is = "+objKal.add());
+        System.out.println("Result Minus is = "+objKal.minus());
+        System.out.println("Result Multiple is = "+objKal.multiplication());
+        System.out.println("Result Division is = "+objKal.division());
+    }
+    
+}
